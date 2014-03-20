f0acfb6f6e848f12276fcf3e81c585913098e1bb
Hieu Huynh <hhyakfish@gmail.com>
2014-03-20 06:08:34 -0700

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Certification::Application.routes.draw do
   
   resources :sessions, only: [:new, :create, :destroy]
   
-  match '/signin', 	to: "sessions#new",			via: "get"
+  match '/signin', 	to: "sessions#new",				via: "get"
   match '/signout', to: "sessions#destroy",		via: "delete"
 
   # The priority is based upon order of creation: first created -> highest priority.
