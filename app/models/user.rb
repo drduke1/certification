7798d9e1e7e3fe4b7d3edf6438feb613c5697b3a
root <root@certification.grandstream.com>
2014-04-23 13:40:28 -0700

    Users have admin and permission levels

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
 	has_many :questions
+	serialize :permission
 
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :permission, presence: true
