ddd69f15313833543b4b8ce8ddf4fed59fa266b2
David Duke <david.duke@grandstream.com>
2014-11-20 09:20:04 -0800

    Products unique, view edits

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ActiveRecord::Base
   has_many :sections, through: :product_sections
   accepts_nested_attributes_for :sections
   
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :category, presence: true
 end
